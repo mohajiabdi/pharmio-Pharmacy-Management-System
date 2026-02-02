15c71bd5db6c2ab66d0055016501150f664ce1f9
mohajiabdi <mo.haji.abdi1@gmail.com>
2026-02-02 13:23:36 +0300

    deleted

--- pharmacy-frontend/src/App.jsx
+++
@@ -1,6 +0,0 @@
-// src/App.jsx
-import AppRoutes from "./routes/AppRoutes";
-
-export default function App() {
-  return <AppRoutes />;
-}
--- pharmacy-frontend/src/components/layout/PublicLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function PublicLayout() {
-  return (
-    <div className="min-h-screen bg-[hsl(var(--background))] text-[hsl(var(--foreground))]">
-      <Outlet />
-    </div>
-  );
-}
